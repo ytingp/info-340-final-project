967fabc1033f6e0884da295f1286e4d075665f1b
Carson Essabhoy <cjessabhoy@gmail.com>
2022-03-08 18:45:00 -0800

    Groups can be added to firebase database

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,47 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Nav from './Nav';
 import Header from './Header';
 import SearchBar from './SearchBar';
 import GroupList from './Groups';
 import CreateGroup from './GroupCreator';
 
+import { getDatabase, ref, set as firebaseSet, push as firebasePush, onValue } from 'firebase/database'
+import { cleanup } from '@testing-library/react';
+
 function App(props) {
+
+  const db = getDatabase();
+
   const [groups, setGroups] = useState(props.groups);
 
+  useEffect(() => {
+    const allGroupsRef = ref(db, "allGroups");
+
+    const offFunction = onValue(allGroupsRef, (snapshot) => {
+      const allGroupsObject = snapshot.val();
+      const groupKeysArray = Object.keys(allGroupsObject);
+
+      const allGroupsArray = groupKeysArray.map((keyString) => {
+        const newGroup = allGroupsObject[keyString];
+        return newGroup;
+      });
+
+      setGroups(allGroupsArray);
+    })
+    console.log(groups);
+
+    function cleanup () {
+      offFunction();
+    }
+
+    return cleanup;
+  }, [])
+
   const addGroup = (newGroup) => {
-    let groupCopy = [...groups];
-    groupCopy.push(newGroup);
-    console.log(groupCopy);
-    setGroups(groupCopy);
+    const allGroupsRef = ref(db, "allGroups");
+    firebasePush(allGroupsRef, newGroup); //Add to array in database
   }
+
 
   return (
     <>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,27 @@ import './index.css';
 import App from './components/App';
 import SAMPLE_GROUPS from './data/groups.json'; //a sample list of GROUPS
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC7rWCvdJ15Yy-V5cH0ZzRC0xbdnztkMMs",
+  authDomain: "looking-for-games-b8.firebaseapp.com",
+  projectId: "looking-for-games-b8",
+  storageBucket: "looking-for-games-b8.appspot.com",
+  messagingSenderId: "469255977088",
+  appId: "1:469255977088:web:d73eb9b1922eed96d00e65",
+  measurementId: "G-WFB0Z26TGZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 ReactDOM.render(
   <React.StrictMode>
     <App groups={SAMPLE_GROUPS} />
