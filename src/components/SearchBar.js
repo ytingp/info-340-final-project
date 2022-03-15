537a6ac072fc7ab4ea8134636c1c45d235bbbfab
ericyu18 <ericyu18@uw.edu>
2022-03-14 17:39:18 -0700

    added third party library

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,3 +1,5 @@
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 export default function SearchBar(props) {
   function handleInput(evt) {
     let lowerCase = evt.target.value.toLowerCase();
@@ -5,6 +7,7 @@ export default function SearchBar(props) {
   }
   return (
     <form id="search">
+      <i className="fa-solid fa-magnifying-glass"></i>
       <label htmlFor="search">Search</label>
       <input type="text" placeholder="Search for games.." name="search" onChange={handleInput}></input>
     </form>
